874de7ab602970fadd24b397f20943acacaeace9
Chethiya2001 <chethiyabandaraherath@gmail.com>
2025-06-03 06:43:18 +0530

    searching done

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,27 +1,36 @@
-import { Component,inject,Inject } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { Component, inject, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Nav } from '../nav/nav';
 import { HousingLocation } from '../housing-location/housing-location';
 import { Footer } from '../footer/footer';
 import { Housing } from '../housing';
 import { HousingLocationInfo } from '../housinglocation';
 
-
-
 @Component({
   selector: 'app-home',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule, HousingLocation],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-housingLocationList:HousingLocationInfo[] = []
-hosingService : Housing = inject(Housing)
+  housingLocationList: HousingLocationInfo[] = [];
+  hosingService: Housing = inject(Housing);
+  filterdHouseLocationList: HousingLocationInfo[] = [];
 
-
-constructor() {
+  constructor() {
     this.housingLocationList = this.hosingService.getAllHouseLocations();
-}
- 
+    this.filterdHouseLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filterdHouseLocationList = this.housingLocationList;
+    }
+    this.filterdHouseLocationList = this.housingLocationList.filter(
+      (h) =>
+      
+        h?.city.toLowerCase().includes(text.toLocaleLowerCase())||
+         h?.state.toLowerCase().includes(text.toLocaleLowerCase())
+    );
+  }
 }
